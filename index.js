24c96a242bbe9a55796b0974b2867b46eaf28a7e
Marcelo <marcelo.megale@fatec.sp.gov.br>
2022-03-09 15:12:30 -0300

    configuramos o dot env

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // https://api.openweathermap.org/data/2.5/forecast?q=Itu&appid=33cf0eef456f1664e3984e0cef1659af
 
+require('dotenv').config()
+
+const axios = require ('axios')
+
+console.log(process.env.APP_KEY)
+
+const {APP_KEY, KEY1, KEY2, KEY3} = process.env
+
+console.log(APP_KEY, KEY1, KEY2, KEY3)
